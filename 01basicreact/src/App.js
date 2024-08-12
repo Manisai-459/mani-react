d481fce2fd0b7a7f1ee38a9a89f548f97c72617f
Mani sai Y <112387763+Manisai-459@users.noreply.github.com>
2024-08-12 23:45:54 +0530

    secondcommit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+
+import Name from "./name";
+function App() {
+  return (
+    <>
+      <h1>Hello World </h1>
+      <Name/>
+    </>
+  );
+}
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Hello mova</h1>
+  )
+}
+
+export default App
